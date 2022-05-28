9000636b513a689457c4984ec8fe4076c239a71c
harsha239 <79088376+harsha239@users.noreply.github.com>
2022-05-28 19:32:31 +0530

    fix

--- attendance_system_facial_recognition/settings.py
+++ attendance_system_facial_recognition/settings.py
@@ -104,43 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-        },
-    },
-    'handlers': {
-        'sentry': {
-            'level': 'WARNING',
-            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
-        },
-        'console': {
-            'level': 'WARNING',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['sentry'],
-            'level': 'WARNING',
-            'propagate': True,
-        },
-        'raven': {
-            'level': 'WARNING',
-            'handlers': ['sentry'],
-            'propagate': False,
-        },
-        'sentry.errors': {
-            'level': 'WARNING',
-            'handlers': ['sentry'],
-            'propagate': False,
-        },
-    }
-}
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
